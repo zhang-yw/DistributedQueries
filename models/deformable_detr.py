a972fb3e41b40e8de46fda3c9645a53f4d676958
yweizhang <zhang2486@wisc.edu>
2022-07-26 15:32:27 -0500

    f

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -392,8 +392,6 @@ class SetCriterion(nn.Module):
         diameter = 2 * radius + 1
         gaussian = self.gaussian2D((diameter, diameter), sigma=diameter / 6)
         gaussian = torch.from_numpy(gaussian)
-        print(gaussian)
-        exit(0)
         x, y = int(center[0]), int(center[1])
 
         height, width = heatmap.shape[0:2]
