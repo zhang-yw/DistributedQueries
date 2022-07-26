7647a0187c0eda65d249d14cd6b8e4f47decc3ae
yweizhang <zhang2486@wisc.edu>
2022-07-26 03:31:56 -0500

    f

--- models/deformable_detr.py
+++ models/deformable_detr.py
@@ -419,6 +419,7 @@ class SetCriterion(nn.Module):
 
         losses = {'loss_hm': self.crit(outputs['pred_hms'], hms)}
         print(losses)
+        exit(0)
         # if 'aux_outputs' in outputs:
         #     for i, aux_outputs in enumerate(outputs['aux_outputs']):
         #         indices = self.matcher(aux_outputs, targets)
