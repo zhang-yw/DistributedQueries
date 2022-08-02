61b0e355cdfbff105b92cf3a4d5b46b715fafd55
yweizhang <zhang2486@wisc.edu>
2022-08-01 21:58:41 -0500

    f

--- generate_vis.py
+++ generate_vis.py
@@ -17,10 +17,108 @@ import shutil, random, os
 import json
 from pycocotools.coco import COCO
 from datasets.coco import make_coco_transforms, ConvertCocoPolysToMask
+import numpy as np
 random.seed(0)
 val_path = "/nobackup/yiwei/coco/images/val2017"
 save_path = "/nobackup/yiwei/coco/images/detr_focal_loss_hm"
 # save_path_2 = "/nobackup/yiwei/coco/images/20_conddetr_att"
+
+def gaussian_radius(det_size, min_overlap=0.7):
+    height, width = det_size
+
+    a1  = 1
+    b1  = (height + width)
+    c1  = width * height * (1 - min_overlap) / (1 + min_overlap)
+    sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
+    r1  = (b1 + sq1) / 2
+
+    a2  = 4
+    b2  = 2 * (height + width)
+    c2  = (1 - min_overlap) * width * height
+    sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
+    r2  = (b2 + sq2) / 2
+
+    a3  = 4 * min_overlap
+    b3  = -2 * min_overlap * (height + width)
+    c3  = (min_overlap - 1) * width * height
+    sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
+    r3  = (b3 + sq3) / 2
+    return min(r1, r2, r3)
+
+def draw_gaussian(heatmap, center, sigma):
+    tmp_size = sigma * 3
+    mu_x = int(center[0] + 0.5)
+    mu_y = int(center[1] + 0.5)
+    w, h = heatmap.shape[0], heatmap.shape[1]
+    ul = [int(mu_x - tmp_size), int(mu_y - tmp_size)]
+    br = [int(mu_x + tmp_size + 1), int(mu_y + tmp_size + 1)]
+    if ul[0] >= h or ul[1] >= w or br[0] < 0 or br[1] < 0:
+        return heatmap
+    size = 2 * tmp_size + 1
+    x = np.arange(0, size, 1, np.float32)
+    y = x[:, np.newaxis]
+    x0 = y0 = size // 2
+    g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2))
+    g_x = max(0, -ul[0]), min(br[0], h) - ul[0]
+    g_y = max(0, -ul[1]), min(br[1], w) - ul[1]
+    img_x = max(0, ul[0]), min(br[0], h)
+    img_y = max(0, ul[1]), min(br[1], w)
+    heatmap[img_y[0]:img_y[1], img_x[0]:img_x[1]] = np.maximum(
+        heatmap[img_y[0]:img_y[1], img_x[0]:img_x[1]],
+        g[g_y[0]:g_y[1], g_x[0]:g_x[1]])
+    return heatmap
+
+def gaussian2D(shape, sigma=1):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m+1,-n:n+1]
+
+    h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    return h
+
+def draw_umich_gaussian(heatmap, center, radius, k=1):
+    diameter = 2 * radius + 1
+    gaussian = gaussian2D((diameter, diameter), sigma=diameter / 6)
+    gaussian = torch.from_numpy(gaussian)
+    x, y = int(center[0]), int(center[1])
+
+    height, width = heatmap.shape[0:2]
+
+    left, right = min(x, radius), min(width - x, radius + 1)
+    top, bottom = min(y, radius), min(height - y, radius + 1)
+
+    masked_heatmap  = heatmap[y - top:y + bottom, x - left:x + right]
+    masked_gaussian = gaussian[radius - top:radius + bottom, radius - left:radius + right]
+    if min(masked_gaussian.shape) > 0 and min(masked_heatmap.shape) > 0: # TODO debug
+        torch.maximum(masked_heatmap, masked_gaussian * k, out=masked_heatmap)
+    return heatmap
+
+def _neg_loss(pred, gt):
+    ''' Modified focal loss. Exactly the same as CornerNet.
+        Runs faster and costs a little bit more memory
+        Arguments:
+        pred (batch x c x h x w)
+        gt_regr (batch x c x h x w)
+    '''
+    pos_inds = gt.eq(1).float()
+    neg_inds = gt.lt(1).float()
+
+    neg_weights = torch.pow(1 - gt, 4)
+
+    loss = 0
+
+    pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
+    neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
+
+    num_pos  = pos_inds.float().sum()
+    pos_loss = pos_loss.sum()
+    neg_loss = neg_loss.sum()
+
+    if num_pos == 0:
+        loss = loss - neg_loss
+    else:
+        loss = loss - (pos_loss + neg_loss) / num_pos
+    return loss
 
 # COCO classes
 CLASSES = [
@@ -179,7 +277,7 @@ for fname in filenames:
     # print(dec_attn_weights[5].shape)
     # print(outputs['pred_hms'][0].view(h,w).shape)
 
-    fig, axs = plt.subplots(ncols=3, nrows=1, squeeze=False, figsize=(5, 5))
+    fig, axs = plt.subplots(ncols=4, nrows=1, squeeze=False, figsize=(5, 5))
     colors = COLORS * 100
 
     for row in range(1):
@@ -209,14 +307,23 @@ for fname in filenames:
                 ax.imshow(dec_attn_weights[0][0,0,:].view(h, w))
                 ax.set_title(f"decoder")
                 ax.axis('off')
-            else:
+            elif col ==2:
                 # print(outputs['pred_hms'][0].view(h, w).numpy())
                 # exit(0)
                 ax.imshow(outputs['pred_hms'][0].view(h, w).numpy())
                 ax.set_title(f"output")
                 ax.axis('off')
-    fig.tight_layout()
-    plt.savefig(os.path.join(save_path, fname))
+            else:
+                radius = gaussian_radius((math.ceil(h), math.ceil(w)))
+                radius = max(0, int(radius))
+                target = target['boxes']
+                hm = torch.zeros((h, w))
+                for j in range(target.size()[0]):
+                    ct = np.array([target[j][0]*w, target[j][1]*h], dtype=np.float32)
+                    ct_int = ct.astype(np.int32)
+                    draw_umich_gaussian(hm, ct_int, radius)
+                    fig.tight_layout()
+                    plt.savefig(os.path.join(save_path, fname))
 
     # for ax_i in axs.T:
     #     ax = ax_i[0]
