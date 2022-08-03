f54ea9c6af1ef4f3db02b1ced25aebdff2983ea8
yweizhang <zhang2486@wisc.edu>
2022-08-03 01:15:37 -0500

    f

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def main(args):
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
 
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         model_without_ddp = model.module
 
     if args.dataset_file == "coco_panoptic":
