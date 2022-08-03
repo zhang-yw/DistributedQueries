c0552961527d806d1c6c01b6ae7a83377ae20d8a
yweizhang <zhang2486@wisc.edu>
2022-08-03 00:46:43 -0500

    f

--- generate_vis.py
+++ generate_vis.py
@@ -261,6 +261,21 @@ for fname in filenames:
         # model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
         #     lambda self, input, output: enc_attn_weights.append(output[1])
         # ),
+        model.transformer.decoder.layers[0].multihead_attn.register_forward_hook(
+            lambda self, input, output: dec_attn_weights.append(output[1])
+        ),
+        model.transformer.decoder.layers[1].multihead_attn.register_forward_hook(
+            lambda self, input, output: dec_attn_weights.append(output[1])
+        ),
+        model.transformer.decoder.layers[2].multihead_attn.register_forward_hook(
+            lambda self, input, output: dec_attn_weights.append(output[1])
+        ),
+        model.transformer.decoder.layers[3].multihead_attn.register_forward_hook(
+            lambda self, input, output: dec_attn_weights.append(output[1])
+        ),
+        model.transformer.decoder.layers[4].multihead_attn.register_forward_hook(
+            lambda self, input, output: dec_attn_weights.append(output[1])
+        ),
         model.transformer.decoder.layers[5].multihead_attn.register_forward_hook(
             lambda self, input, output: dec_attn_weights.append(output[1])
         ),
